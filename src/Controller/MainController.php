051b03aabb845ba1bf239c973e52bbbde55af787
Soczux <m.soczynska@onet.pl>
2021-05-27 14:51:49 +0200

    Redirect / to /songs

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,8 +13,6 @@ class MainController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->redirectToRoute('songs');
     }
 }
