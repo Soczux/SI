3523bf2787bd60695caacd2e94642c1375d8ed11
Soczux <m.soczynska@onet.pl>
2021-06-22 14:38:24 +0200

    Add User Controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/account", name="account")
+     *
+     * @IsGranted("ROLE_USER")
+     */
+    public function account(): Response
+    {
+        return $this->render('user/account.html.twig');
+    }
+}
